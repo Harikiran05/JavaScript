33f7b3078fa197426fadc540c2db45c35cf875e1
Harikiran05 <harikirandowpati@gmail.com>
2026-08-26 17:38:42 +0530

    Tests using Jasmine

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -30,6 +30,7 @@ const dateString = deliveryDate.format('dddd, MMMM D');
   cartSummaryHTML +=
   `
   <div class="cart-item-container 
+  js-cart-item-container
   js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: ${dateString}
@@ -46,7 +47,7 @@ const dateString = deliveryDate.format('dddd, MMMM D');
                 <div class="product-price">
                   $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                   <span>
                     Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">
                     ${cartItem.quantity}</span>
@@ -58,7 +59,7 @@ const dateString = deliveryDate.format('dddd, MMMM D');
                   <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">
                     Save
                   </span>
-                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                  <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
