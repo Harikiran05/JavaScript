c6247b78d0105d6558ea37acdc343de14905dd9f
Harikiran05 <harikirandowpati@gmail.com>
2026-08-29 15:47:08 +0530

    Learned promises

--- javascript-amazon-project-main/javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/data/cart.js
@@ -81,3 +81,21 @@ export function updateQuantity(productId, newQuantity){
   matchingItem.deliveryOptionId = deliveryOptionId;
   saveToStorage();
 }
+
+
+export function loadCart(fun){
+  const xhr = new XMLHttpRequest();
+
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response)
+
+    fun();
+  });
+
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
+
+
